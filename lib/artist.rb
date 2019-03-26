d6b841165b7ad9faf59bdc79b698b48ab7ec8801
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-26 21:11:44 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def new_song(song_name, genre)
+    song = Song.new(song_name, self, genre)
+    song
+  end
+  
+  def songs 
+    
+  end
+  
+  def genres 
+    
+  end
+  
+end
